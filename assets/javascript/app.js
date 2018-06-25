5b7acac1289bc2085f1fb1fd9365636936527982
Kwong <aykwong93@gmail.com>
2018-06-25 16:26:16 -0400

    Fixed submit button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,25 +3,25 @@ var button = {
   omdb: ["Frozen", "Inception", "Iron Man", "The Matrix"]
 };
 
+//Creates the buttons depending on the active tab, empties and recreates as necessary
 function create() {
+  $(".card-body").empty();
   if ($(".nav-link:first").hasClass("active")) {
     console.log("inside 1");
     var array = button.giphy;
+    var type = "giphy";
   } else {
     console.log("inside 2");
     var array = button.omdb;
+    var type = "omdb";
   }
   let topicHolder = $.trim($("#topicsInput").val());
-  $(".card-body").empty();
   if (topicHolder !== "") {
     array.push(topicHolder);
+    console.log(array);
   }
   for (let count = 0; count < array.length; count++) {
-    if ($(".nav-link:first").hasClass("active")) {
-      var topicButton = $('<button class="choice giphy">');
-    } else {
-      var topicButton = $('<button class="choice omdb">');
-    }
+    var topicButton = $(`<button class="choice ${type}">`);
     $(topicButton).addClass("btn btn-outline-dark");
     $(topicButton).attr("data-name", array[count]);
     $(topicButton).text(array[count]);
@@ -30,6 +30,7 @@ function create() {
   $("#topicsInput").val("");
 }
 
+//Utilizes the giphy website and outputs the content into the HTML page
 function giphy() {
   let search = $(this).attr("data-name");
   let queryURL = `https://api.giphy.com/v1/gifs/search?api_key=jKcKvRwB8fihgLsGnwfLTndJmcbs6v67&q=${search}&limit=10&lang=en`;
@@ -37,14 +38,14 @@ function giphy() {
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
+  }).then(function (response) {
     for (let number = 0; number < response.data.length; number++) {
       let source = response.data[number];
       let gif = $('<div class="group">');
       let rating = $("<p>").text(`Rating: ${source.rating.toUpperCase()}`);
       let image = $(
         `<img src="${source.images.fixed_height_still.url}" alt="${
-          source.title
+        source.title
         }" />`
       );
       $(image).attr("state", "still");
@@ -57,6 +58,7 @@ function giphy() {
   });
 }
 
+//Utilizes the OMDB website and outputs the content into the HTML page
 function omdb() {
   let search = $(this).attr("data-name");
   let queryURL = `http://www.omdbapi.com/?t=${search}&apikey=ad83c563`;
@@ -64,9 +66,9 @@ function omdb() {
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
+  }).then(function (response) {
     let movie = $("<div>");
-    let source = $(`<p class="title">`).text(response.Title);
+    let source = $(`<h3 class="title">`).text(response.Title);
     let released = $(`<p class="released">`).text(response.Released);
     let rating = $(`<p class="rating">`).text(response.Rating);
     let plot = $(`<p class="plot">`).text(response.Plot);
@@ -77,6 +79,7 @@ function omdb() {
   });
 }
 
+//Animates the giphys when user clicks on the image
 function animate() {
   let state = $(this).attr("state");
   $(this).attr("state", "animate");
@@ -87,20 +90,31 @@ function animate() {
   }
 }
 
+//Changes the tab CSS and satisfies conditions for array choice, proceeds with button creation
 function active() {
   $(".nav-link").removeClass("active");
   $(this).addClass("active");
   create();
 }
 
+//Prevents page from refreshing on Submit button, proceeds with button creation
+function preventRefresh(event) {
+  event.preventDefault();
+  create();
+}
+
+//Creates pre-set buttons on page load
 $(document).ready(create);
 
-$("#add-topic").on("click", create);
+//Creates buttons from user input
+$("#add-topic").on("click", preventRefresh);
 
+//Run in intended API server/website
 $(document).on("click", ".giphy", giphy);
-
 $(document).on("click", ".omdb", omdb);
 
+//Runs animate function on image click
 $("#output").on("click", "img", animate);
 
+//Changes the button array and nav-tab on click
 $(".card").on("click", ".nav-link", active);
